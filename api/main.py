42259be687301d25c20f3fbb7309316d9cef29a8
Guido De Rosa <guidoderosa@gmail.com>
2024-06-06 02:18:11 +0100

    "static UI" :)

--- api/main.py
+++ api/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .routers import easyrsa
 
 
 app = FastAPI()
 
+app.mount("/ui", StaticFiles(directory="ui"), name="ui")
 
 app.include_router(easyrsa.router)
 
