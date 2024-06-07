fbe498e0ac3c6e9f65834d175d3e78e422080b9c
Guido De Rosa <guidoderosa@gmail.com>
2024-06-07 03:56:36 +0100

    get ip addr from system!

--- api/fast/routers/network/interfaces.py
+++ api/fast/routers/network/interfaces.py
@@ -1,3 +1,6 @@
+import subprocess
+import json
+
 from typing import List
 
 from fastapi import APIRouter
@@ -16,8 +19,30 @@ class NetworkInterface(BaseModel):
     ip: IPData
 
 
+def get_network_interfaces():
+    iproute2_data = json.loads(subprocess.check_output(['ip', '--json', 'address', 'show']))
+    netifs = []
+    for iproute2_iface in iproute2_data:
+        netif = dict(
+            name=iproute2_iface['ifname'],
+            ip=dict(
+                addresses=list()
+            )
+        )
+        for addr_info_el in iproute2_iface['addr_info']:
+            netif['ip']['addresses'].append(dict(
+                addr=addr_info_el['local'],
+                prefix=addr_info_el['prefixlen']
+            ))
+        netifs.append(netif)
+    return netifs
 
-network_interfaces = [
+
+
+network_interfaces = get_network_interfaces()
+
+"""
+    [
         {
             "name": "lo",
             "ip": {
@@ -46,7 +71,7 @@ network_interfaces = [
             }
         }
     ]
-
+"""
 
 router = APIRouter(
     tags=["network/interfaces"],
