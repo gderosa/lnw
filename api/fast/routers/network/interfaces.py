3f1f3156cb662d6f93ae41e5262071889e6fb185
Guido De Rosa <guidoderosa@gmail.com>
2024-06-08 18:52:45 +0100

    ip addr and ips as PyDantic objs

--- api/fast/routers/network/interfaces.py
+++ api/fast/routers/network/interfaces.py
@@ -7,8 +7,9 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 
+
 class IPAddress(BaseModel):
-    addr: str
+    addr: str | None
     prefix: int | None
 
 class IPData(BaseModel):
@@ -19,27 +20,32 @@ class NetworkInterface(BaseModel):
     ip: IPData
 
 
-def get_network_interfaces():
+
+def get_network_interfaces() -> NetworkInterface:
     iproute2_data = json.loads(subprocess.check_output(['ip', '--json', 'address', 'show']))
     netifs = []
     for iproute2_iface in iproute2_data:
-        netif = dict(
-            name=iproute2_iface['ifname'],
-            ip=dict(
-                addresses=list()
+        netif = NetworkInterface(
+            name = iproute2_iface['ifname'],
+            ip = IPData(
+                addresses = []
             )
         )
         for addr_info_el in iproute2_iface['addr_info']:
-            netif['ip']['addresses'].append(dict(
+            netif.ip.addresses.append(IPAddress(
                 addr=addr_info_el['local'],
                 prefix=addr_info_el['prefixlen']
             ))
         netifs.append(netif)
     return netifs
 
-
-
-network_interfaces = get_network_interfaces()
+def set_network_interfaces(netifs: List[NetworkInterface]):
+    old_netifs = get_network_interfaces()
+    for old_netif in old_netifs:
+        netif = [ni for ni in netifs if ni.name == old_netif.name][0]
+        for address in netif.ip.addresses:
+            if address in old_netif.ip.addresses:
+                print(f"ip remove {address} from {netif.name}")
 
 
 
@@ -48,13 +54,11 @@ router = APIRouter(
     prefix="/api/v1"
 )
 
-
 @router.get("/network/interfaces")
 async def read_netifs():
-    return network_interfaces
+    return get_network_interfaces()
 
 @router.put("/network/interfaces")
 async def replace_netifs(netifs: List[NetworkInterface]):
-    global network_interfaces
-    network_interfaces = netifs
-    return []
+    set_network_interfaces(netifs)
+    return get_network_interfaces()
