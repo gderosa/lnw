5441dca6da1a1bf470e6a83ca9f055c45758ba29
lnw <guidoderosa@gmail.com>
2024-10-06 04:49:38 +0100

    handle already existing lnw user

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-APP=lnw
-DATADIR=/var/lib/$APP
-HOMEDIR=$DATADIR
-USER=$APP
-GROUP=$USER
-PASSWORD=pass
-APPDIR=/opt/$APP
+APPNAME=lnw
+DATADIR=/var/lib/$APPNAME
+USERNAME=$APPNAME
+GROUPNAME=$USERNAME
+PASSWORD=$USERNAME
+APPDIR=`dirname $0`/..
 APIDIR=$APPDIR/api/fast
-VENVDIR=$HOMEDIR/.virtualenvs/$APP
 PYTHON=python3
 
 export DEBIAN_FRONTEND=noninteractive
@@ -21,21 +19,25 @@ apt-get -y autoremove
 apt-get -y install sudo
 apt-get -y install python3 python3-venv
 
-adduser --system --shell /bin/bash --home $HOMEDIR --group $GROUP && \
-        echo "$USER:$PASSWORD" | chpasswd
+(adduser --system --shell /bin/bash --home $DATADIR --group $GROUPNAME  && \
+        echo "$USERNAME:$PASSWORD" | chpasswd )                             || \
+        mkdir -p $DATADIR  # if the user already exists, the HOME may be different
 
-chmod o-rwx $HOMEDIR
+HOMEDIR=$(su - $USERNAME -c 'echo $HOME')
+VENVDIR=$HOMEDIR/.virtualenvs/$APPNAME
 
-install -v $APPDIR/scripts/files/sudoers /etc/sudoers.d/$USER
+chmod o-rwx $DATADIR
 
-su - $USER -c "$PYTHON -m venv $VENVDIR"
-su - $USER -c "
+install -v $APPDIR/scripts/files/sudoers /etc/sudoers.d/$USERNAME
+
+su $USERNAME -c "$PYTHON -m venv $VENVDIR"
+su $USERNAME -c "
     cd $APIDIR/
     source $VENVDIR/bin/activate
     pip install -r requirements.txt
 "
 
-systemctl enable systemd-networkd
-systemctl start systemd-networkd
-systemctl status systemd-networkd
+# systemctl enable systemd-networkd
+# systemctl start systemd-networkd
+# systemctl status systemd-networkd --no-pager
 
