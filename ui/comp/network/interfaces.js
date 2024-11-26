b0819caa5dcaf528dbf451daff5ab6112afc7c10
Guido De Rosa <guidoderosa@gmail.com>
2024-11-26 18:15:42 -0300

    netif table! (really basic)

--- ui/comp/network/interfaces.js
+++ ui/comp/network/interfaces.js
@@ -5,11 +5,33 @@ class NetworkInterfaces extends HTMLElement {
         super();       
     }
     async connectedCallback() {
+        const table = $new('table');
+        let tHead = $new('thead');
+        tHead.innerHTML = `
+            <tr>
+                <th>Name</th>
+                <th>Type</th>
+            </tr>
+        `;
+        const tBody = $new('tbody');
         const response = await fetch('/api/v1/network/interfaces');
         const jsonData = await response.json();
-        const pre = $new('pre');
-        pre.textContent = JSON.stringify(jsonData, {}, 4);
-        this.appendChild(pre);
+        jsonData.forEach((netIf) => {
+            const tr = $new('tr');
+
+            const name = $new('td');
+            name.textContent = netIf.name;
+            tr.appendChild(name);
+
+            const linkType = $new('td');
+            linkType.textContent = netIf.link_type;
+            tr.appendChild(linkType);
+
+            tBody.appendChild(tr);
+        })
+        table.appendChild(tHead);
+        table.appendChild(tBody);
+        this.appendChild(table);
     }
 }
 customElements.define('network-interfaces', NetworkInterfaces);
