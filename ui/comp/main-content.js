6e1a50ccac14f54e6a241c828c7ec5335b7fc96b
Guido De Rosa <guidoderosa@gmail.com>
2024-11-25 23:11:54 -0300

    why this is window???

--- ui/comp/main-content.js
+++ ui/comp/main-content.js
@@ -1,1 +1,18 @@
-// window.navigation.addEventListener('navigate', (e) => { console.log(e.destination.url) } )
+import { $new } from "../lib/dom.js"
+
+class MainContent extends HTMLElement {
+    constructor() {
+        super();       
+    }
+    connectedCallback() {
+        window.addEventListener("popstate", this.router);
+    }
+    router(event) {
+        const hash = event.currentTarget.document.location.hash;
+        console.log(hash);
+        const pre = $new('pre');
+        pre.textContent = hash;
+        console.log(this);
+    }
+}
+customElements.define('main-content', MainContent)
