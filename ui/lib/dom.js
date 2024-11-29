589736852b21b00e478c7cca15c3d482032ea407
Guido De Rosa <guidoderosa@gmail.com>
2024-11-29 13:12:11 -0300

    more aliases

--- ui/lib/dom.js
+++ ui/lib/dom.js
@@ -1,13 +1,14 @@
 const   $byId   = (id)                          => document.getElementById(id);
 const   $ID     = $byId;
+
 const   $byQs   = (selector, element=document)  => element.querySelector(selector);
+const   $qs     = $byQs;
 const   $sel    = $byQs;
 
 const   $node   = (idOrSelector)                => $byId(idOrSelector) || $byQs(idOrSelector);
 const   $       = $node;
 
-
 const   $new    = (tag)                         => document.createElement(tag);
 
 
-export {$byId, $ID, $byQs, $sel, $node, $, $new};
+export { $byId, $ID, $byQs, $qs, $sel, $node, $, $new };
