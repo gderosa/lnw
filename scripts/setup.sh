693e08e76905aa0f33687fe177cb6155fb7ac00d
Guido De Rosa <guidoderosa@gmail.com>
2024-12-09 00:49:52 -0300

    do not migrate if Debian interfaces not there

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -45,7 +45,9 @@ systemctl unmask systemd-networkd
 systemctl start systemd-networkd
 systemctl unmask systemd-resolved
 systemctl start systemd-resolved
-ENABLE_TEST_COMMANDS=1 netplan migrate
+if [ -f /etc/network/interfaces ]; then
+    ENABLE_TEST_COMMANDS=1 netplan migrate
+fi
 apt-get -y purge avahi-daemon ifupdown resolvconf
 chmod -v go-rwx /etc/netplan/*.yaml
 netplan apply
