a1cad4cc1f4e8339541f84525f3d50b5ff60aa23
Guido De Rosa <guidoderosa@gmail.com>
2024-12-09 20:09:34 -0300

    more host color id... II

--- ui/comp/main-container.js
+++ ui/comp/main-container.js
@@ -5,12 +5,14 @@ class MainContainer extends HTMLElement {
         super();       
     }
     async connectedCallback() {
-        const opacityHex = '12';
+        const opacityHexBg = '08';
+        const opacityHexFg = 'bb';
         const response = await fetch('/api/v1/system/info');
         const systemInfo = await response.json();
         const machineHash = systemInfo.machine_hash;
-        const cssFgColor = '#' + machineHash.slice(-6);
-        const cssBgColor = cssFgColor + opacityHex;
+        const cssColor = '#' + machineHash.slice(-6);
+        const cssBgColor = cssColor + opacityHexBg;
+        const cssFgColor = cssColor + opacityHexFg;
         document.styleSheets[0].insertRule(`table tr td { background: ${cssBgColor} }`, 0);
         document.styleSheets[0].insertRule(`side-menu a { color: ${cssFgColor} }`, document.styleSheets[0].cssRules.length);
         // window.dispatchEvent(new CustomEvent('system-info', systemInfo));
